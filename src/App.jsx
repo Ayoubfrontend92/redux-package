4f85776592746428a5c0bed62eb12fb9971425c8
ayoub rezaei <ayoub.frontend1992@gmail.com>
2023-01-22 12:43:10 -0800

    redux : action .reducer,store

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './App.css'
+import Counter from './components/Counter'
+import { Provider } from 'react-redux'
+import store from './redux/store'
+
+
+const App = () => {
+  return (
+    <Provider store={store}>
+       <div>
+        <Counter/>
+       </div>
+    </Provider>
+   
+  )
+}
+
+export default App
